52645678f912fffd1914db1fbb01092de02c594e
[julie-ddeng] <julie.deng2@mail.mcgill.ca>
2026-02-08 15:17:48 -0500

    inverse method trial 1

--- src/rsa/KeyGen.java
+++ src/rsa/KeyGen.java
@@ -5,8 +5,8 @@ public class KeyGen {
     private int p;
     private int q;
     private int modulus;
-    private int privateKey;
     private int publicKey;
+    private int privateKey;
     private int eulers;
 
 
@@ -18,7 +18,6 @@ public class KeyGen {
         this.eulers = (this.p - 1) * (this.q - 1);
         this.publicKey = generatePublic();
         this.privateKey = generatePrivate();
-
     }
 
     // getters
@@ -36,10 +35,9 @@ public class KeyGen {
 
 
     // Public/private key generator functions
-
     private int gcd(int a, int b){
-        /* This function calculates and returns the greatest common divisor (GCD) of two integers a and b using
-        Euclid's algorithm.
+        /** This function calculates and returns the greatest common divisor (GCD) of two integers a and b using
+         * Euclid's algorithm.
          */
         int r = a % b;
         while(r != 0) {
@@ -49,9 +47,11 @@ public class KeyGen {
         }
         return b;
     }
+
     private int generatePublic(){
-        /* This function generates a public key k, such that the gcd( k, (p-1)*(q-1) ) = 1, where (p-1)*(q-1) is
-        Euler's Totient. */
+        /** This function generates a public key k, such that the gcd( k, (p-1)*(q-1) ) = 1, where (p-1)*(q-1) is
+         * Euler's Totient.
+         */
 
         Random randomGen = new Random();
         int k;
@@ -62,26 +62,53 @@ public class KeyGen {
             if (k % 2 == 0 || k == this.p || k == this.q )
                 continue;
             // Check if relatively prime
-            if (gcd(this.eulers, k) == 1)
+            if (gcd(this.eulers, k) == 1){
                 break;
+            }
         }
         return k;
     }
 
-    private int generatePrivate(){
-        /* This function generates a private key s such that it is the inverse of the public key k, where the modulus
-        is Euler's Totient.
+    private int inverse(int n, int k){
+        /** This function finds and returns the multiplicative inverse of an integer k, modulus n, using the extended
+         * Euclidean algorithm.
          */
-        int s = 1;
+        int r = n % k;
+        int q = (n - r) / k;
+        int t1 = 0;
+        int t2 = 1;
+        int t3 = t1 - q * t2;
+        while(r != 0) {
+            n = k;
+            k = r;
+            r = n % k;
+            q = (n - r) / k;
+            t1 = t2;
+            t2 = t3;
+            t3 = t1 - q * t2;
+        }
+        return t2;
+    }
 
+    private int generatePrivate(){
+        /** This function generates a private key s such that it is the inverse of the public key k, where the modulus
+         * is Euler's Totient.
+         */
+        int s;
+        s = inverse(this.eulers, this.publicKey);
+
+        // Ensures s is in the range [0, modulus]
+        if (s < 0)
+            s = s + this.eulers;
         return s;
     }
+
+
 
     public static void main(String[] args){
         KeyGen newKey = new KeyGen(13, 11);
         System.out.println(newKey.publicKey);
-
-
+        System.out.println(newKey.privateKey);
 
     }
 }
